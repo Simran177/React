9408024af353e79c4a713e14fb5ac546d6944d23
Simran <simran.maurya_cs18@gla.ac.in>
2021-02-15 21:45:40 +0530

    React components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import { Component } from "react";
+import FC from "./Components/functionalComponent";  //single component import from dafault of that file
+import {ClassComponent,ClassComponent1} from "./Components/ClassComponent"; //multilpe imports
+import SnippetsComponent from "./Components/SnippetsComponent";
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <FC />
+      <ClassComponent/>
+      <ClassComponent1/>
+      <SnippetsComponent/>
+
+     
     </div>
   );
 }
---
+++ src/Components/ClassComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export class ClassComponent extends React.Component
+{
+    render(){
+        return(
+            <h1>This is my class Component</h1>
+        )
+    }
+}
+export class ClassComponent1 extends React.Component
+{
+    render(){
+        return(
+            <h1>This is another Component</h1>
+        )
+    }
+}
+//export default ClassComponent;
