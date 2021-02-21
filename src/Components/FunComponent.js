ef110c9e525a70380898f400602ae3883e9176c9
Simran <simran.maurya_cs18@gla.ac.in>
2021-02-21 19:36:28 +0530

    function component

---
+++ src/Components/FunComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+//in function component no need to write render method
+function FunComponent()
+{
+    return <h1>This is my Functional Component</h1>;
+    
+}
+
+//we use export statement to perform nesting
+export default FunComponent;
