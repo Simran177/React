2646ba56ffcbd3da206ecf718a91f3fd63df466a
Simran <simran.maurya_cs18@gla.ac.in>
2021-02-24 15:08:21 +0530

    React State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import './Practical-7/App1.css';
+import './App.css';
 import ClassComponent from './Practical-7/ClassComponent';
 import FunctionalComponent from './Practical-7/FunctionalComponent';
 import ReactFragment from './Practical-7/ReactFragment';
+import ReactState from './ReactState';
 
 
 
 function App() {
   return (
-    <>
-     
-      <div className="function">
-      <FunctionalComponent/>
-      </div>
-      <div className="class">
-      <ClassComponent/>
-      </div>
-
-      </>
-   
+    <div className="App">
+      <ReactState roll={11} name="Manish"/>
+    </div>
   ); 
 }
-
 export default App;
