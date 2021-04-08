0f2d84115283bfc8d4035035e6058da98733b8a0
Simran <simran.maurya_cs18@gla.ac.in>
2021-04-08 18:45:50 +0530

    react router dom package added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import DynamicTable from "./Practical-9/DynamicTable";
-
-
+import NavBar from "./Components/React Router/NavBar";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./Components/React Router/Home";
+import About from "./Components/React Router/About";
+import Contact from "./Components/React Router/Contact";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import NotFound from "./Components/React Router/NotFound";
+import "./Components/React Router/App.css";
 function App() {
 
   return (
-  <DynamicTable/>
-
-  );
+    <Router>
+  <div className="App">
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/about" component={About}></Route>
+      <Route exact path="/contact" component={Contact}></Route>
+      <Route component={NotFound}></Route>
+    </Switch>
+    </div>
+  </Router>
+);
 
 }
 
